cb3a475d4c5eec821130b32923fe12f0fdf80cc1
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-02-14 09:06:47 +0100

    Create three columns to show the employees. Add names and roles and images of the employees.

--- main.py
+++ main.py
@@ -12,3 +12,30 @@ purus. Nullam eget faucibus est. Phasellus a vulputate justo. Praesent ac
 orci consequat, elementum erat vitae, feugiat mauris.
 """
 st.write(company_description)
+
+st.subheader("Our Team")
+
+col1, empty_col1, col2, empty_col2, col3 = st.columns([1.5, 0.5, 1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv")
+
+with col1:
+    for index, row in df[:4].iterrows():
+        st.header(f"{row['first name'].capitalize()} "
+                  f"{row['last name'].capitalize()}")
+        st.write(row["role"])
+        st.image("images/" + row["image"])
+
+with col2:
+    for index, row in df[4:8].iterrows():
+        st.header(f"{row['first name'].capitalize()} "
+                  f"{row['last name'].capitalize()}")
+        st.write(row["role"])
+        st.image("images/" + row["image"])
+
+with col3:
+    for index, row in df[8:].iterrows():
+        st.header(f"{row['first name'].capitalize()} "
+                  f"{row['last name'].capitalize()}")
+        st.write(row["role"])
+        st.image("images/" + row["image"])
