7167e7d57e340c6bf8a12166f6375cc68e1acecb
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-02-16 10:09:35 +0100

    Finished Contact Us Form with features: choose the topic, write a message and email us.

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas
+from send_email import send_email
 
 st.header("Contact Us")
 
@@ -8,9 +9,17 @@ df = pandas.read_csv("topics.csv")
 with st.form("contact_us", clear_on_submit=True):
     user_email = st.text_input("Your email address")
     topic = st.selectbox("What topic do you want to discuss?", df, key="topic")
-    message = st.text_area("Your message")
+    raw_message = st.text_area("Your message")
+
+    message = f"""\
+Subject: New email from Company Website
+
+From: {user_email}
+Topic: {topic}
+{raw_message}
+"""
 
     submitted = st.form_submit_button("Submit")
     if submitted:
-        print(topic)
+        send_email(message)
         st.info("Message submitted")
